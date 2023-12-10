1e19de0f5771177d3f2583b711e8229d8458990f
Filip Jerga <jerga99@gmail.com>
2023-12-10 12:08:26 +0000

    Pan handler animation

--- src/components/activity/Item.js
+++ src/components/activity/Item.js
@@ -1,22 +1,45 @@
 
 
 
-import { StyleSheet } from "react-native"
+import { Animated, PanResponder, StyleSheet } from "react-native"
 import { FlowHighlightView, FlowRow, FlowText } from "../overrides"
 import { COLORS } from "../../variables/styles"
+import { useRef } from "react"
 
 export const ActivityItem = ({title}) => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null, {dx: pan.x, dy: pan.y}], {useNativeDriver: false}),
+      onPanResponderRelease: () => {
+        pan.extractOffset();
+      }
+    })
+  ).current
+
   return (
-    <FlowHighlightView style={styles.itemContainer}>
-      <FlowRow style={styles.row}>
-        <FlowText>
-          {title}
-        </FlowText>
-        <FlowText style={styles.time}>
-          00:00:00
-        </FlowText>
-      </FlowRow>
-    </FlowHighlightView>
+    <Animated.View
+      {...panResponder.panHandlers}
+      style={{
+        touchAction: "none",
+        userSelect: "none",
+        transform: [{translateX: pan.x}]
+      }}
+    >
+      <FlowHighlightView style={styles.itemContainer}>
+        <FlowRow style={styles.row}>
+          <FlowText>
+            {title}
+          </FlowText>
+          <FlowText style={styles.time}>
+            00:00:00
+          </FlowText>
+        </FlowRow>
+      </FlowHighlightView>
+    </Animated.View>
   )
 }
 
