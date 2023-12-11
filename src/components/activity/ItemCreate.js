6d2099dfcfd078a57b082671c5d9e41fe733cd45
Filip Jerga <jerga99@gmail.com>
2023-12-11 12:00:39 +0000

    Add input to modal

--- src/components/activity/ItemCreate.js
+++ src/components/activity/ItemCreate.js
@@ -1,15 +1,49 @@
-import { FlowModal, FlowText } from "../overrides"
-
+import { Pressable, StyleSheet, TextInput } from "react-native";
+import { FlowModal, FlowRow, FlowText } from "../overrides";
+import { COLORS } from "../../variables/styles";
 
 
 export const ItemCreate = () => {
+
+  const confirm = () => {
+    console.log("Confirm!");
+  };
+
+  const cancel = () => {
+    console.log("Cancel!");
+  };
 
   return (
     <FlowModal
       visible={true}
       animationType={"fade"}
     >
-      <FlowText>Hi There!</FlowText>
+      <FlowText>Choose the name of the activity.</FlowText>
+      <TextInput
+        style={styles.input}
+        placeholder="Learn C#"
+        placeholderTextColor={COLORS.semiDarkGray}
+      />
+      <FlowRow>
+        <Pressable onPress={confirm}>
+          <FlowText>Confirm</FlowText>
+        </Pressable>
+        <Pressable onPress={cancel}>
+          <FlowText>Cancel</FlowText>
+        </Pressable>
+      </FlowRow>
     </FlowModal>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    color: COLORS.white,
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: COLORS.semiDarkGray,
+    borderRadius: 5,
+    marginVertical: 10
+  }
+});
