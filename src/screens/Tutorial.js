4d1526678ccea5e1819bfecee9c82c4eeff0e02c
Filip Jerga <jerga99@gmail.com>
2023-12-13 14:31:27 +0000

    Stepping

--- src/screens/Tutorial.js
+++ src/screens/Tutorial.js
@@ -2,8 +2,25 @@ import { View } from "react-native"
 import { FlowButton, FlowModal, FlowRow, FlowText } from "../components/overrides"
 import { useState } from "react"
 
+const MAX_STEPS = 3;
+
 export const TutorialScreen = ({visible}) => {
   const [step, setStep] = useState(1);
+
+  const canGoNext = step < MAX_STEPS;
+  const canGoBack = step > 1;
+
+  const goNext = () => {
+    if (canGoNext) {
+      setStep(step + 1);
+    }
+  }
+
+  const goBack = () => {
+    if (canGoBack) {
+      setStep(step - 1);
+    }
+  }
 
   return (
     <FlowModal visible={visible}>
@@ -32,12 +49,16 @@ export const TutorialScreen = ({visible}) => {
       <View>
         <FlowRow>
           <FlowButton
+            onPressIn={goBack}
+            disabled={!canGoBack}
             ghost
             type={"primary"}
             content={"Back"}
             style={{marginRight: 10}}
           />
           <FlowButton
+            onPressIn={goNext}
+            disabled={!canGoNext}
             ghost
             type={"primary"}
             content={"Next"}
